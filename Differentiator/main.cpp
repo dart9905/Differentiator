118b5fe1c5f1db7b9df50041b2a65ceb4fc3e3b6
Khromov Alex <31626058+dart9905@users.noreply.github.com>
2017-11-25 20:25:03 +0300

    A1.0mac

---
+++ Differentiator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Differentiator
+//
+//  Created by macbook on 25.11.2017.
+//  Copyright © 2017 macbook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
